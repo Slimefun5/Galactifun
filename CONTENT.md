b0309e689b70eeb8a072d6c3052c16ff21342c87
intisy (Finn Birich) <finn@birich.de>
2026-06-30 13:26:20 +0200

    chore: point Discord links to the new community server

--- CONTENT.md
+++ CONTENT.md
@@ -4,4 +4,4 @@ Inspiration taken from GalactiCraft. It has a large API for addon devs.
 
 Feel free to join us on the Slimefun Addon Community Discord:
 
-https://discord.gg/SqD3gg5SAU
+https://discord.gg/CbBYZBEWdR
--- src/main/java/io/github/addoncommunity/galactifun/Galactifun.java
+++ src/main/java/io/github/addoncommunity/galactifun/Galactifun.java
@@ -137,7 +137,7 @@ public final class Galactifun extends AbstractAddon {
                 "",
                 "Galactifun is open source, you can contribute or report bugs at: ",
                 getBugTrackerURL(),
-                "Join the Slimefun Addon Community Discord: discord.gg/SqD3gg5SAU",
+                "Join the Slimefun Addon Community Discord: discord.gg/CbBYZBEWdR",
                 "",
                 "###################################################"
         ));
